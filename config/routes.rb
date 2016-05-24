e223434ab4428add9cf4cd6ea1a7476ea6a3b8d3
Chiel Hackman <chielhackman@gmail.com>
2016-05-24 15:13:24 +0200

    Cleaned up the code a bit and working on new feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, :only => [:show]
+
   resources :contacts do
     resources :favourites, :works, :friends, :familys
   end
+
   root to: "contacts#index"
 
   get 'user_contacts/:user_id' => 'contacts#user', as: :user_contacts
   get 'user_favourites/:user_id' => 'contacts#favourites', as: :user_favourites
 
+  get 'users' => 'users#index'
+  get 'users/:id', :to => 'users#show'
 end
